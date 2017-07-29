a0543b06dae717519f889ab2d7262a28b012bac0
Suyeol Jeon <devxoul@gmail.com>
2017-07-30 00:29:04 +0900

    Add isEnabled to AlertActionType

--- Sources/AlertActionType.swift
+++ Sources/AlertActionType.swift
@@ -3,10 +3,15 @@ import UIKit
 public protocol AlertActionType: Equatable {
   var title: String { get }
   var style: UIAlertActionStyle { get }
+  var isEnabled: Bool { get }
 }
 
 public extension AlertActionType {
   var style: UIAlertActionStyle {
     return .default
+  }
+
+  var isEnabled: Bool {
+    return true
   }
 }
